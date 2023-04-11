0b1fcc8c8ec417545ca0be2f80502165892977f1
shareef67 <129213895+shareef67@users.noreply.github.com>
2023-04-11 18:02:31 -0500

    Add files via upload

---
+++ cmds-2.sql
@@ -0,0 +1,9 @@
+LOAD DATA LOCAL INFILE 'c:/Users/draj0/Videos/Avatar_Pandora.csv' INTO table Avatar_Pandora FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'c:/Users/draj0/Videos/Distributors.csv' INTO table Distributors FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'c:/Users/draj0/Videos/Sales.csv' INTO table Sales FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'c:/Users/draj0/Videos/OrderTbl.csv' INTO table OrderTbl FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'c:/Users/draj0/Videos/Employee.csv' INTO table Employee FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
